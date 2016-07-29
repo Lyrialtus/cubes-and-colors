ca19f384badd3f3bd6bd9528e7cbfb89969b8e7e
Pavel Mokin <lyrialtus@gmail.com>
2016-07-29 21:46:00 +0300

    Major update
    All global variables were added to constructor.
    Raycasting and coloring were completely rewritten.
    This resolves #1.

--- cubes.js
+++ cubes.js
@@ -1,51 +1,50 @@
 'use strict';
 
-let scene;
-let camera;
-let renderer;
-let controls;
-let raycaster;
-let mouse;
-let cubes;
-
 class Animation {
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = [];
+    this.controls = [];
+    this.renderer = [];
+    this.mouse = new THREE.Vector2();
+    this.raycaster = new THREE.Raycaster();
+    this.cubes = new THREE.Group();
+    this.allVertices = [];
+  }
+
   init() {
 
     // For N cubes (from 2 to 8)
     const CUBES_NUM = Math.floor(Math.random() * 7) + 2;
 
-    // Scene and camera
-    scene = new THREE.Scene();
-
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.z = 70 + CUBES_NUM * 10;
+    // Camera
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    this.camera.position.z = 70 + CUBES_NUM * 10;
 
     // Navigation
-    controls = new THREE.TrackballControls(camera);
-    controls.rotateSpeed = 1.0;
-    controls.zoomSpeed = 0.5;
-    controls.panSpeed = 0.8;
-    controls.noZoom = false;
-    controls.noPan = false;
-    controls.dynamicDampingFactor = 0.3;
+    this.controls = new THREE.TrackballControls(this.camera);
+    this.controls.rotateSpeed = 1.0;
+    this.controls.zoomSpeed = 0.5;
+    this.controls.panSpeed = 0.8;
+    this.controls.noZoom = false;
+    this.controls.noPan = false;
+    this.controls.dynamicDampingFactor = 0.3;
 
     // Lights
     let directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
     directionalLight.position.set(0, 0.5, 1);
-    scene.add(directionalLight);
+    this.scene.add(directionalLight);
 
     let AmbientLight = new THREE.AmbientLight(0x777777);
-    scene.add(AmbientLight);
+    this.scene.add(AmbientLight);
 
     // Renderer
-    renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer = new THREE.WebGLRenderer({ alpha: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
     let container = document.getElementById('container');
-    container.appendChild(renderer.domElement);
+    container.appendChild(this.renderer.domElement);
 
     // Cubes
-    cubes = new THREE.Group();
-
     for (let i = 0; i < CUBES_NUM; i++) {
 
       let cube = new THREE.Group();
@@ -61,49 +60,48 @@ class Animation {
       for (let i = 0; i < positions.length; i++) {
         let geometry1 = new THREE.SphereGeometry(1, 16, 16);
 
-        // Random colors!
+        // Random colors
         let material1 = new THREE.MeshPhongMaterial({ color: Math.random() * 0xffffff });
         let sphere = new THREE.Mesh(geometry1, material1);
         sphere.position.set(positions[i].x, positions[i].y, positions[i].z);
         cubeVertices.add(sphere);
       }
 
-      // Edges of the cube
-      let cubeEdges = new THREE.Group();
+      // Raycaster will use it
+      this.allVertices.push(...cubeVertices.children);
 
-      // Four cylinders for y-axis
+      // Edges of the cube: four cylinders for y-axis
       let geometry2 = new THREE.CylinderGeometry(0.5, 0.5, 10, 32);
       let material2 = new THREE.MeshPhongMaterial({ color: 0x777777 });
 
       let cylinder = new THREE.Mesh(geometry2, material2); // 1
       cylinder.translateX(5);
       cylinder.translateZ(5);
-      let edgeY = new THREE.Group();
-      edgeY.add(cylinder);
+      let edgesY = new THREE.Group();
+      edgesY.add(cylinder);
 
       cylinder = cylinder.clone(); // 2
       cylinder.translateZ(-10);
-      edgeY.add(cylinder);
+      edgesY.add(cylinder);
 
       cylinder = cylinder.clone(); // 3
       cylinder.translateX(-10);
-      edgeY.add(cylinder);
+      edgesY.add(cylinder);
 
       cylinder = cylinder.clone(); // 4
       cylinder.translateZ(10);
-      edgeY.add(cylinder);
+      edgesY.add(cylinder);
 
       // Adding edges for x-axis
-      let edgeX = edgeY.clone();
-      edgeX.rotateZ(Math.PI / 2);
+      let edgesX = edgesY.clone();
+      edgesX.rotateZ(Math.PI / 2);
 
       // Adding edges for z-axis
-      let edgeZ = edgeY.clone();
-      edgeZ.rotateX(Math.PI / 2);
+      let edgesZ = edgesY.clone();
+      edgesZ.rotateX(Math.PI / 2);
 
       // Final composition of the cube
-      cubeEdges.add(edgeX, edgeY, edgeZ);
-      cube.add(cubeVertices, cubeEdges);
+      cube.add(cubeVertices, edgesX, edgesY, edgesZ);
 
       cube.position.y = Math.random() * 60 - 30;
       cube.position.z = Math.random() * 60 - 30;
@@ -113,17 +111,15 @@ class Animation {
       cube.rotation.y = Math.random() * 2 * Math.PI;
       cube.rotation.z = Math.random() * 2 * Math.PI;
 
-      cubes.add(cube);
+      this.cubes.add(cube);
     }
 
-    scene.add(cubes);
+    this.scene.add(this.cubes);
 
     // Useful option
     window.addEventListener('resize', this.onWindowResize, false);
 
-    // Edge coloring
-    raycaster = new THREE.Raycaster();
-    mouse = new THREE.Vector2();
+    // For clicks
     document.addEventListener('mousedown', this.onDocumentMouseDown, false);
 
     // For touches
@@ -131,72 +127,52 @@ class Animation {
   }
 
   onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    animation.camera.aspect = window.innerWidth / window.innerHeight;
+    animation.camera.updateProjectionMatrix();
+    animation.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
   onDocumentTouchStart(event) {
     event.preventDefault();
     event.clientX = event.touches[0].clientX;
     event.clientY = event.touches[0].clientY;
-    this.onDocumentMouseDown(event);
+    animation.onDocumentMouseDown(event);
   }
 
   onDocumentMouseDown(event) {
     event.preventDefault();
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-    // Tricky raycasting!
-    raycaster.setFromCamera(mouse, camera);
-    let intersect = [];
-    let number = [];
-    for (let i = 0; i < cubes.children.length; i++) {
+    animation.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    animation.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-      let intersects = raycaster.intersectObject(cubes.children[i].children[0], true);
+    // Raycasting and coloring
+    animation.raycaster.setFromCamera(animation.mouse, animation.camera);
+    let intersects = animation.raycaster.intersectObjects(animation.allVertices);
 
-      if (intersects.length > 0) {
-        intersect.push(intersects[0]);
-        number.push(i);
-      }
-    }
-
-    let col;
-    if (intersect.length === 1) {
-      // Simple case
-      col = intersect[0].object.material.color;
-      cubes.children[number].children[1].children[0].children[0].material.color.set(col);
-    } else if (intersect.length > 1) {
-      // Prevention of simultaneous coloring
-      if (intersect[0].distance > intersect[1].distance) {
-        col = intersect[intersect.length - 1].object.material.color;
-        cubes.children[number[number.length - 1]].children[1]
-            .children[0].children[0].material.color.set(col);
-      } else {
-        col = intersect[0].object.material.color;
-        cubes.children[number[0]].children[1].children[0].children[0].material.color.set(col);
-      }
+    if (intersects.length > 0) {
+      let col = intersects[0].object.material.color;
+      let parentCube = intersects[0].object.parent.parent;
+      parentCube.children[1].children[0].material.color.set(col);
     }
   }
 
   animate() {
-    requestAnimationFrame(this.animate.bind(this));
-    this.render();
+    requestAnimationFrame(animation.animate);
+    animation.render();
   }
 
   render() {
 
-    for (let i = 0; i < cubes.children.length; i++) {
+    for (let i = 0; i < this.cubes.children.length; i++) {
 
       // Independent rotation
-      cubes.children[i].rotation.x += -0.002;
-      cubes.children[i].rotation.y += -0.002;
-      cubes.children[i].rotation.z += -0.002;
+      this.cubes.children[i].rotation.x += -0.002;
+      this.cubes.children[i].rotation.y += -0.002;
+      this.cubes.children[i].rotation.z += -0.002;
     }
 
-    controls.update();
-    renderer.render(scene, camera);
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
   }
 }
 
